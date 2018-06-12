00bfea1dccaaaeb66892476c8140650fbcc50a21
解园园 <xieyuanyuan@unovo.com.cn>
2018-06-12 17:21:33 +0800

    fix: 整理文件

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import babel from 'rollup-plugin-babel';
-export default {
+import pkg from './package.json';
+
+export default [{
     input: './src/index.js',
     output: {
         file: 'dist/bundle.js',
@@ -11,4 +13,9 @@ export default {
             exclude: 'node_modules/**'
         })
     ]
-};
+}, {
+    input: './src/index.js',
+    output: [
+        {file: pkg.main, format: 'cjs'}
+    ]
+}];
